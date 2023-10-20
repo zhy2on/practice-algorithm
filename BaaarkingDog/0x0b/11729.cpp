c5be5674b279c62230468b8a2ef79b409e7517c0
zhy2on <dhwlgus5940@gmail.com>
2023-10-20 21:56:09 +0900

    feat: 11729

---
+++ BaaarkingDog/0x0b/11729.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void hanoi(int a, int b, int n) {
+	if (n == 1) {
+		cout << a << ' ' << b << '\n';
+		return;
+	}
+	hanoi(a, 6 - a - b, n - 1);
+	cout << a << ' ' << b << '\n';
+	hanoi(6 - a - b, b, n - 1);
+}
+
+int main(void) {
+	int n;
+
+	cin >> n;
+	cout << (1 << n) - 1 << '\n';
+	hanoi(1, 3, n);
+}
