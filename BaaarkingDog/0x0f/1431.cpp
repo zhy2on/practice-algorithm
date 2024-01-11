1f259534c5936933b4b5cda880b938b0863f4249
zhy2on <dhwlgus5940@gmail.com>
2024-01-11 17:34:22 +0900

    feat: 1431

--- BaaarkingDog/0x0f/1431.cpp
+++ BaaarkingDog/0x0f/1431.cpp
@@ -5,19 +5,14 @@ using namespace std;
 int N;
 string arr[51];
 
-bool comp_length(const string &a, const string &b) {
-	return a.length() < b.length();
-}
-
-bool comp_cnt(const string &a, const string &b) {
+bool comp(const string &a, const string &b) {
+	int alen = a.length(), blen = b.length();
 	int acnt = 0, bcnt = 0;
-	for (auto c : a) {
-		if (c >= '0' && c <= '9') acnt += c - '0';
-	}
-	for (auto c : b) {
-		if (c >= '0' && c <= '9') bcnt += c - '0';
-	}
-	return acnt < bcnt;
+	if (alen != blen) return alen < blen;
+	for (auto c : a) if (c >= '0' && c <= '9') acnt += c - '0';
+	for (auto c : b) if (c >= '0' && c <= '9') bcnt += c - '0';
+	if (acnt != bcnt) return acnt < bcnt;
+	return a < b;
 }
 
 int main(void) {
@@ -26,10 +21,6 @@ int main(void) {
 
 	cin >> N;
 	for (int i = 0; i < N; ++i) cin >> arr[i];
-
-	sort(arr, arr + N);
-	stable_sort(arr, arr + N, comp_cnt);
-	stable_sort(arr, arr + N, comp_length);
-
+	sort(arr, arr + N, comp);
 	for (int i = 0; i < N; ++i) cout << arr[i] << '\n';
 }
