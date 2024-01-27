3378cf353492f1a65410f2925b122391de296447
zhy2on <dhwlgus5940@gmail.com>
2024-01-27 14:39:10 +0900

    feat: 10825

--- BaaarkingDog/0x0f/10825.cpp
+++ BaaarkingDog/0x0f/10825.cpp
@@ -4,14 +4,7 @@
 
 using namespace std;
 
-#define student tuple<string, int, int, int>
-
-bool comp(const student &a, const student &b) {
-	if (get<1>(a) != get<1>(b)) return get<1>(a) > get<1>(b);
-	if (get<2>(a) != get<2>(b)) return get<2>(a) < get<2>(b);
-	if (get<3>(a) != get<3>(b)) return get<3>(a) > get<3>(b);
-	return get<0>(a) < get<0>(b);
-}
+#define student tuple<int, int, int, string>
 
 int main(void) {
 	ios::sync_with_stdio(0);
@@ -23,8 +16,8 @@ int main(void) {
 		int a, b, c;
 		string s;
 		cin >> s >> a >> b >> c;
-		arr[i] = {s, a, b, c};
+		arr[i] = {-a, b, -c, s};
 	}
-	sort(arr, arr + N, comp);
-	for (int i = 0; i < N; ++i) cout << get<0>(arr[i]) << '\n';
+	sort(arr, arr + N);
+	for (int i = 0; i < N; ++i) cout << get<3>(arr[i]) << '\n';
 }
