53a9fbffb40cf5446dcca8d1f311f1449ac59ae9
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-03-10 23:59:42 +0900

    1541

--- BaaarkingDog/0x11/1541.cpp
+++ BaaarkingDog/0x11/1541.cpp
@@ -9,13 +9,13 @@ int main(void) {
 
 	cin >> str;
 	int tmp = 0, multi = 1, ans = 0;
-	for (int i = 0; i < str.length(); ++i) {
-		if (str[i] >= '0' && str[i] <= '9')
-			tmp = tmp * 10 + str[i] - '0';
+	for (char c : str) {
+		if (c >= '0' && c <= '9')
+			tmp = tmp * 10 + c - '0';
 		else {
 			ans += tmp * multi;
 			tmp = 0;
-			if (multi == 1 && str[i] == '-') multi = -1;
+			if (multi == 1 && c == '-') multi = -1;
 		}
 	}
 	ans += tmp * multi;
