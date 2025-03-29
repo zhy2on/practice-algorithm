f122a0f6e598f68d8e30bf11f38ead15a904ba0d
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-03-29 16:00:10 +0900

    2493 ver2

--- BaaarkingDog/2nd/0x05/2493.cpp
+++ BaaarkingDog/2nd/0x05/2493.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 stack<pair<int, int> > s;
-int ans[500001];
 
 int main(void) {
 	ios::sync_with_stdio(0);
@@ -11,23 +10,14 @@ int main(void) {
 
 	cin >> n;
 
+	s.push({100000001, 0});
+
 	for (int i = 1; i <= n; ++i) {
 		int t;
 		cin >> t;
 
-		while (!s.empty() && s.top().first < t) {
-			int idx = s.top().second;
-			s.pop();
-			if (!s.empty()) ans[idx] = s.top().second;
-		}
+		while (s.top().first < t) s.pop();
+		cout << s.top().second << ' ';
 		s.push({t, i});
 	}
-
-	while (!s.empty()) {
-		int idx = s.top().second;
-		s.pop();
-		if (!s.empty()) ans[idx] = s.top().second;
-	}
-
-	for (int i = 1; i <= n; ++i) cout << ans[i] << ' ';
 }
