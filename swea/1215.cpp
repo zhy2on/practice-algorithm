8b16ce4999a29c157e92ec726658129c9a3da992
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-05-10 21:22:10 +0900

    1215

--- swea/1215.cpp
+++ swea/1215.cpp
@@ -2,21 +2,30 @@
 
 using namespace std;
 
+bool is_palin_row(string board[8], int i, int l, int r) {
+	while (l < r) {
+		if (board[i][l] != board[i][r]) return false;
+		l++;
+		r--;
+	}
+	return true;
+}
+
+bool is_palin_col(string board[8], int j, int l, int r) {
+	while (l < r) {
+		if (board[l][j] != board[r][j]) return false;
+		l++;
+		r--;
+	}
+	return true;
+}
+
 int find_palindrome(int n, string board[8]) {
 	int cnt = 0;
 	for (int i = 0; i < 8; ++i) {
 		for (int j = 0; j < 8; ++j) {
-			int k = 0;
-			for (k = 0; (n + j - 1) < 8 && k < n / 2; ++k) {
-				if (board[i][j + k] != board[i][(n + j - 1) - k]) break;
-			}
-			if (k == n / 2) ++cnt;
-
-			k = 0;
-			for (k = 0; (n + i - 1) < 8 && k < n / 2; ++k) {
-				if (board[i + k][j] != board[(n + i - 1) - k][j]) break;
-			}
-			if (k == n / 2) ++cnt;
+			if (j + n - 1 < 8 && is_palin_row(board, i, j, j + n - 1)) ++cnt;
+			if (i + n - 1 < 8 && is_palin_col(board, j, i, i + n - 1)) ++cnt;
 		}
 	}
 	return cnt;
