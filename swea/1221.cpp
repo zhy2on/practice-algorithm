8aea1e19b2ba2852daa8c29d076e5f50c3ac6c54
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-05-12 23:41:13 +0900

    1221

--- swea/1221.cpp
+++ swea/1221.cpp
@@ -1,20 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const string number[10] = {"ZRO", "ONE", "TWO", "THR", "FOR",
-						   "FIV", "SIX", "SVN", "EGT", "NIN"};
-
-int which_num(string str) {
-	int i;
-	for (i = 0; i < 10; ++i) {
-		if (number[i] == str) break;
-	}
-	return i;
-}
+const string NUMBER_STRINGS[10] = {"ZRO", "ONE", "TWO", "THR", "FOR",
+								   "FIV", "SIX", "SVN", "EGT", "NIN"};
+unordered_map<string, int> STRING_TO_NUM;
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+
+	for (int i = 0; i < 10; ++i) {
+		STRING_TO_NUM[NUMBER_STRINGS[i]] = i;
+	}
+
 	int t;
 	cin >> t;
 	for (int i = 0; i < t; ++i) {
@@ -23,19 +21,20 @@ int main(void) {
 
 		cin >> t_num >> n;
 
-		int ns[100000];
+		int n_count[10];
 
+		cout << t_num << '\n';
+		fill(n_count, n_count + 10, 0);
 		for (int j = 0; j < n; ++j) {
 			string str;
 			cin >> str;
-			ns[j] = which_num(str);
+			n_count[STRING_TO_NUM[str]]++;
 		}
 
-		sort(ns, ns + n);
-
-		cout << t_num << '\n';
-		for (int j = 0; j < n; ++j) {
-			cout << number[ns[j]] << ' ';
+		for (int j = 0; j < 10; ++j) {
+			for (int k = 0; k < n_count[j]; ++k) {
+				cout << NUMBER_STRINGS[j] << ' ';
+			}
 		}
 		cout << '\n';
 	}
