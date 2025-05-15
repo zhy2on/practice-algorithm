d690015f253f3dce8f112a89af9fb64b90c55255
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-05-15 23:57:22 +0900

    1954

---
+++ swea/1954.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+int main(void) {
+	int t;
+	cin >> t;
+	for (int t_c = 0; t_c < t; ++t_c) {
+		int n;
+		cin >> n;
+
+		cout << '#' << t_c + 1 << '\n';
+
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+			}
+			cout << '\n';
+		}
+	}
+}
