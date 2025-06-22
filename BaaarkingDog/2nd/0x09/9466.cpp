6d1cec1e08e34681147a268ba1bdcf280b41b5e4
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-06-22 19:38:49 +0900

    9466 정답

--- BaaarkingDog/2nd/0x09/9466.cpp
+++ BaaarkingDog/2nd/0x09/9466.cpp
@@ -1,42 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, s[100001], state[100001], cnt;
+
+const int NOT_VISITED = -1;
+const int IN_CYCLE = 0;
+
+void mark_cycle(int start) {
+	int cur = s[start];
+	state[cur] = IN_CYCLE;
+	++cnt;
+
+	while (cur != start) {
+		cur = s[cur];
+		state[cur] = IN_CYCLE;
+		++cnt;
+	}
+}
+
+void run() {
+	cin >> n;
+	cnt = 0;
+
+	for (int i = 1; i <= n; ++i) {
+		cin >> s[i];
+		state[i] = NOT_VISITED;
+	}
+
+	for (int i = 1; i <= n; ++i) {
+		if (state[i] != NOT_VISITED) continue;
+
+		int cur = i;
+		while (1) {
+			state[cur] = i;
+			cur = s[cur];
+
+			if (state[cur] == i) {	// 사이클 발견
+				mark_cycle(cur);
+				break;
+			}
+			if (state[cur] != NOT_VISITED) break;  // 이미 탐색했었다면
+		}
+	}
+	cout << n - cnt << '\n';
+}
+
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
 	int T;
 	cin >> T;
-	while (T--) {
-		int s[1000001], vis[1000001], n;
 
-		cin >> n;
-		for (int i = 1; i <= n; ++i) {
-			cin >> s[i];
-			vis[i] = -1;
-		}
-
-		int ans = 0;
-		for (int i = 1; i <= n; ++i) {
-			int cur = i;
-			while (1) {
-				if (vis[cur] == 1) break;
-				++vis[cur];
-				cur = s[cur];
-			}
-
-			for (int j = 1; j <= n; ++j) {
-				if (!vis[j]) {
-					++ans;
-					vis[j] = 1;
-				}
-			}
-		}
-		cout << ans << '\n';
-	}
+	while (T--) run();
 }
-
-/*
-1 2 3 4 5
-3 4 2 5 4
-*/
