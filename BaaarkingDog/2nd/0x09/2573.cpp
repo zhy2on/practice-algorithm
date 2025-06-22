1534b506436501b4ebe54a5f81b8bcdaffa44f5b
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-06-22 22:12:12 +0900

    2573 실패 ver2

--- BaaarkingDog/2nd/0x09/2573.cpp
+++ BaaarkingDog/2nd/0x09/2573.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
+int n, m, cnt;
 int board[301][301], sea[301][301], vis[301][301];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, -1, 0, 1};
@@ -17,6 +17,9 @@ int main(void) {
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < m; ++j) {
 			cin >> board[i][j];
+
+			// 빙산 count
+			if (board[i][j]) ++cnt;
 		}
 	}
 
@@ -38,17 +41,22 @@ int main(void) {
 			for (int dir = 0; dir < 4; ++dir) {
 				int nx = x + dx[dir], ny = y + dy[dir];
 				if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-				if (sea[nx][ny] && board[x][y]) --board[x][y];
+				if (sea[nx][ny] && board[x][y]) {
+					--board[x][y];
+					if (!board[x][y]) --cnt;
+				}
 			}
 		}
 
 		// 빙산 bfs
+		int tmp = 0;
 		for (int i = 0; i < n; ++i) {
 			for (int j = 0; j < m; ++j) {
 				vis[i][j] = 0;
 				if (board[i][j] && q.empty()) {
 					q.push({i, j});
 					vis[i][j] = 1;
+					++tmp;
 				}
 			}
 		}
@@ -63,17 +71,14 @@ int main(void) {
 
 				q.push({nx, ny});
 				vis[nx][ny] = 1;
+				++tmp;
 			}
 		}
 
-		// 방문 안 한 빙산 있나 확인
-		for (int i = 0; i < n; ++i) {
-			for (int j = 0; j < m; ++j) {
-				if (board[i][j] && !vis[i][j]) {
-					cout << year;
-					return 0;
-				}
-			}
+		// 확인
+		if (tmp != cnt) {
+			cout << year;
+			return 0;
 		}
 	}
 }
