817bcf6ada85a305c147f05b0fb1dbe04ca5fe66
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-06-28 14:13:00 +0900

    1074

--- BaaarkingDog/2nd/0x0b/1074.cpp
+++ BaaarkingDog/2nd/0x0b/1074.cpp
@@ -7,18 +7,16 @@ ll ans;
 int N, r, c;
 
 void func(int x, int y, int k, ll cnt) {
-	if (ans) return;
-
 	if (k == 1) {
 		if (x == r && y == c) ans = cnt;
 		return;
 	}
 
 	k = k / 2;
-	if (x + k <= r && y + k <= c) func(x + k, y + k, k, cnt + k * k * 3);
-	if (x + k <= r && y <= c) func(x + k, y, k, cnt + k * k * 2);
-	if (x <= r && y + k <= c) func(x, y + k, k, cnt + k * k);
-	if (x <= r && y <= c) func(x, y, k, cnt);
+	if (x + k <= r && y + k <= c) return func(x + k, y + k, k, cnt + k * k * 3);
+	if (x + k <= r && y <= c) return func(x + k, y, k, cnt + k * k * 2);
+	if (x <= r && y + k <= c) return func(x, y + k, k, cnt + k * k);
+	return func(x, y, k, cnt);
 }
 
 int main(void) {
