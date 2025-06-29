61f81edea87513d3b1e88266cbbb31362cd8451d
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-06-29 22:18:56 +0900

    15650 next_permutation ver

--- BaaarkingDog/2nd/0x0c/15650.cpp
+++ BaaarkingDog/2nd/0x0c/15650.cpp
@@ -2,25 +2,20 @@
 using namespace std;
 
 int n, m;
-int ans[10];
-
-void f(int k, int start) {
-	if (k == m) {
-		for (int i = 0; i < m; ++i) cout << ans[i] << ' ';
-		cout << '\n';
-		return;
-	}
-
-	for (int i = start; i <= n; ++i) {
-		ans[k] = i;
-		f(k + 1, i + 1);
-	}
-}
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
 	cin >> n >> m;
-	f(0, 1);
+
+	vector<int> a;
+	for (int i = 0; i < n; ++i) a.push_back(i < m ? 0 : 1);
+
+	do {
+		for (int i = 0; i < n; ++i) {
+			if (a[i] == 0) cout << i + 1 << ' ';
+		}
+		cout << '\n';
+	} while (next_permutation(a.begin(), a.end()));
 }
