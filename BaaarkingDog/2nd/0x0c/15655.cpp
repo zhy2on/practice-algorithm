abcb4f1031decae07478271edf1e9718ee392d3a
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-06-29 22:53:27 +0900

    15655

---
+++ BaaarkingDog/2nd/0x0c/15655.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, arr[10], ans[10];
+
+void f(int k, int st) {
+	if (k == m) {
+		for (int i = 0; i < k; ++i) cout << ans[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for (int i = st; i < n; ++i) {
+		ans[k] = arr[i];
+		f(k + 1, i + 1);
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i) cin >> arr[i];
+	sort(arr, arr + n);
+	f(0, 0);
+}
