0d67cf457801a658c6895734596ca5b94403418b
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-07-11 20:12:05 +0900

    18808 ver2

--- BaaarkingDog/2nd/0x0d/18808.cpp
+++ BaaarkingDog/2nd/0x0d/18808.cpp
@@ -1,55 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int board[40][40], tmp[40][40];
-int sticker[100][4][10][10];
-pair<int, int> rc[100];
-int N, M, K;
+int board[40][40];
+int sticker[10][10];
+int N, M, K, R, C;
 
-bool is_ok(int x, int y, int k, int d) {
-	// board x,y에 스티커 붙일 수 있는가
-	auto [r, c] = rc[k];
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < M; ++j) tmp[i][j] = board[i][j];
+bool put(int x, int y) {
+	for (int i = 0; i < R; ++i) {
+		for (int j = 0; j < C; ++j) {
+			if (x + i >= N || y + j >= M) return false;
+			if (sticker[i][j] && board[x + i][y + j]) return false;
+		}
 	}
 
-	for (int i = 0; i < r; ++i) {
-		for (int j = 0; j < c; ++j) {
-			if (x + i >= N || y + j >= M) return false;
-			if (sticker[k][d][i][j] && tmp[x + i][y + j]) return false;
-			if (sticker[k][d][i][j]) tmp[x + i][y + j] = sticker[k][d][i][j];
+	for (int i = 0; i < R; ++i) {
+		for (int j = 0; j < C; ++j) {
+			if (sticker[i][j]) board[x + i][y + j] = 1;
 		}
 	}
 	return true;
 }
 
-bool simulation(int k, int d) {
-	for (int x = 0; x < N; ++x) {
-		for (int y = 0; y < M; ++y) {
-			if (is_ok(x, y, k, d)) {
-				swap(board, tmp);
-				return true;
+void rotate() {
+	int tmp[10][10] = {};
+
+	for (int i = 0; i < R; ++i) {
+		for (int j = 0; j < C; ++j) {
+			tmp[j][R - 1 - i] = sticker[i][j];
+		}
+	}
+	swap(R, C);
+	for (int i = 0; i < R; ++i) {
+		for (int j = 0; j < C; ++j) {
+			sticker[i][j] = tmp[i][j];
+		}
+	}
+}
+
+void simulation() {
+	for (int d = 0; d < 4; ++d) {
+		if (d > 0) rotate();
+		for (int x = 0; x < N; ++x) {
+			for (int y = 0; y < M; ++y) {
+				if (put(x, y)) return;
 			}
-		}
-	}
-	return false;
-}
-
-void rotate(int k, int d) {
-	swap(rc[k].first, rc[k].second);
-	auto [r, c] = rc[k];
-	for (int i = 0; i < r; ++i) {
-		for (int j = 0; j < c; ++j) {
-			sticker[k][d][i][j] = sticker[k][d - 1][c - j - 1][i];
-		}
-	}
-}
-
-void run() {
-	for (int k = 0; k < K; ++k) {
-		for (int d = 0; d < 4; ++d) {
-			if (d > 0) rotate(k, d);
-			if (simulation(k, d)) break;
 		}
 	}
 }
@@ -60,16 +54,13 @@ int main(void) {
 
 	cin >> N >> M >> K;
 	for (int k = 0; k < K; ++k) {
-		int r, c;
-		cin >> r >> c;
+		cin >> R >> C;
 
-		rc[k] = {r, c};
-		for (int i = 0; i < r; ++i) {
-			for (int j = 0; j < c; ++j) cin >> sticker[k][0][i][j];
+		for (int i = 0; i < R; ++i) {
+			for (int j = 0; j < C; ++j) cin >> sticker[i][j];
 		}
+		simulation();
 	}
-
-	run();
 
 	int cnt = 0;
 	for (int i = 0; i < N; ++i) {
