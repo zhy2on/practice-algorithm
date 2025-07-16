a4b7bf7a647739375e78d81009b2d2a77e14d18d
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-07-16 09:28:59 +0900

    13335 ver2

--- BaaarkingDog/2nd/0x0d/13335.cpp
+++ BaaarkingDog/2nd/0x0d/13335.cpp
@@ -3,22 +3,25 @@ using namespace std;
 
 int n, w, L, ans;
 int truck[1000];
-int bridge[101];
 
 void run() {
+	queue<int> bridge;
 	int curl_l = 0, cur = 0;
+
+	// 큐 초기화 필요
+	for (int i = 0; i < w; ++i) bridge.push(0);
 
 	while (cur < n || curl_l > 0) {	 // 다리 위에 트럭이 남아있을 수도 있음
 		// 매 초마다 다리 한 칸 전진
-		curl_l -= bridge[0];
-		for (int i = 0; i < w - 1; ++i) bridge[i] = bridge[i + 1];
+		curl_l -= bridge.front();
+		bridge.pop();
 
 		// 새로운 트럭을 다리에 올릴 수 있는지 확인
 		if (cur < n && curl_l + truck[cur] <= L) {
-			bridge[w - 1] = truck[cur];
+			bridge.push(truck[cur]);
 			curl_l += truck[cur++];
 		} else {
-			bridge[w - 1] = 0;	// 트럭 못 올라가면 빈칸 유지
+			bridge.push(0);
 		}
 		++ans;
 	}
