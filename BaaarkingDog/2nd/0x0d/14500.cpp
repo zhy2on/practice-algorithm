3b4bb2af3a44a85385abe93f87681c4c754e90ef
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-07-22 17:48:56 +0900

    14500 dfs ver

--- BaaarkingDog/2nd/0x0d/14500.cpp
+++ BaaarkingDog/2nd/0x0d/14500.cpp
@@ -1,61 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dir[3];
 int board[500][500];
 bool vis[500][500];
 
-int N, M, x, y, ans;
+int N, M, summ, ans;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, -1, 0, 1};
 
-void print() {
-  for (int i = 0; i < N; ++i) {
-    for (int j = 0; j < M; ++j) cout << board[i][j] << ' ';
-    cout << '\n';
-  }
-  cout << '\n';
-}
-
-void update() {
-  bool vis[500][500] = {};
-  int summ = board[x][y];
-
-  int nx = x, ny = y;
-  vis[nx][ny] = 1;
-  for (int i = 0; i < 3; ++i) {
-    nx = nx + dx[dir[i]], ny = ny + dy[dir[i]];
-    vis[nx][ny] = 1;
-    if (nx < 0 || nx >= N || ny < 0 || ny >= M) return;
-    summ += board[nx][ny];
-  }
-  ans = max(ans, summ);
-
-  for (int i = 0; i < N; ++i) {
-    for (int j = 0; j < M; ++j) cout << vis[i][j] << ' ';
-    cout << '\n';
-  }
-  cout << '\n';
-}
-
-void dfs(int k) {
-  if (k == 3) {
-    update();
+void dfs(int cx, int cy, int k) {
+  if (k == 4) {
+    ans = max(ans, summ);
     return;
   }
-  for (int i = 0; i < 4; ++i) {
-    if (k > 0 && i == (dir[k - 1] + 2) % 4) continue;
-    dir[k] = i;
-    dfs(k + 1);
+
+  for (int d = 0; d < 4; ++d) {
+    int nx = cx + dx[d], ny = cy + dy[d];
+
+    if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+    if (vis[nx][ny]) continue;
+
+    vis[nx][ny] = 1;
+    summ += board[nx][ny];
+
+    dfs(nx, ny, k + 1);
+    if (k == 2) {
+      dfs(cx, cy, k + 1);
+    }
+
+    vis[nx][ny] = 0;
+    summ -= board[nx][ny];
   }
 }
 
 void run() {
   for (int i = 0; i < N; ++i) {
     for (int j = 0; j < M; ++j) {
-      x = i;
-      y = j;
-      dfs(0);
+      vis[i][j] = 1;
+      summ += board[i][j];
+      dfs(i, j, 1);
+      vis[i][j] = 0;
+      summ -= board[i][j];
     }
   }
 }
