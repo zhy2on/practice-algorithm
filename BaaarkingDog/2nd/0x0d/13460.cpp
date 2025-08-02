59367d0fe55495cfcf95be3b45211fe2636982f4
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-08-02 12:15:15 +0900

    13460 정답git log

--- BaaarkingDog/2nd/0x0d/13460.cpp
+++ BaaarkingDog/2nd/0x0d/13460.cpp
@@ -3,59 +3,83 @@ using namespace std;
 
 #define X first
 #define Y second
+#define RED 1
+#define BLUE 2
 
-pair<int, int> red, blue, red2, blue2;
-char board[10][10], board2[10][10];
 int N, M, ans = INT_MAX;
-int dir[10];
+char board[10][10], board2[10][10];
+pair<int, int> red, blue, red2, blue2;
+bool is_red_fall, is_blue_fall;
+int tilt_d[10];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, -1, 0, 1};
 
-void roll(int &x, int &y, int d) {
+int who_first(int dir) {
+  if (dir == 0) {
+    if (red2.X > blue2.X) return RED;
+    return BLUE;
+  } else if (dir == 1) {
+    if (red2.Y < blue2.Y) return RED;
+    return BLUE;
+  } else if (dir == 2) {
+    if (red2.X < blue2.X) return RED;
+    return BLUE;
+  } else if (dir == 3) {
+    if (red2.Y > blue2.Y) return RED;
+    return BLUE;
+  }
+  return -1;
+}
+
+void roll(int &x, int &y, int dir, bool &is_fall) {
+  char ball = board2[x][y];
+  board2[x][y] = '.';
   while (1) {
-    int nx = x + dx[d], ny = y + dy[d];
-    if (nx < 0 || nx >= N || ny < 0 || ny >= M) return;
-    if (board2[nx][ny] == '.' || board2[nx][ny] == 'O') {
-      x = nx;
-      y = ny;
+    int nx = x + dx[dir];
+    int ny = y + dy[dir];
+    if (board2[nx][ny] != '.') {
+      if (board2[nx][ny] == 'O') is_fall = true;
+      break;
     }
-    if (board2[nx][ny] != '.') return;
+    x = nx;
+    y = ny;
+  }
+  if (!is_fall) board2[x][y] = ball;
+}
+
+void tilt(int dir) {
+  int who = who_first(dir);
+  if (who == RED) {
+    roll(red2.X, red2.Y, dir, is_red_fall);
+    roll(blue2.X, blue2.Y, dir, is_blue_fall);
+  } else {
+    roll(blue2.X, blue2.Y, dir, is_blue_fall);
+    roll(red2.X, red2.Y, dir, is_red_fall);
   }
 }
 
-void tilt(int d) {
-  board2[red2.X][red2.Y] = '.';
-  roll(red2.X, red2.Y, d);
-  if (board2[red2.X][red2.Y] != 'O') {
-    board2[red2.X][red2.Y] = 'R';
-  }
-
-  board2[blue2.X][blue2.Y] = '.';
-  roll(blue2.X, blue2.Y, d);
-  if (board2[blue2.X][blue2.Y] != 'O') {
-    board2[blue2.X][blue2.Y] = 'B';
-  }
+void init() {
+  memcpy(board2, board, sizeof board);
+  red2 = red;
+  blue2 = blue;
+  is_red_fall = is_blue_fall = false;
 }
 
 void dfs(int k) {
   if (k == 10) {
-    blue2 = blue;
-    red2 = red;
-    memcpy(board2, board, sizeof board);
-    for (int i = 0; i < k; ++i) {
-      tilt(dir[i]);
-      if (board2[blue2.X][blue2.Y] == 'O') return;
-      if (board2[red2.X][red2.Y] == 'O') {
-        ans = min(ans, i + 1);
+    init();
+    for (int i = 0; i < 10; ++i) {
+      tilt(tilt_d[i]);
+      if (is_red_fall || is_blue_fall) {
+        if (is_red_fall && !is_blue_fall) ans = min(ans, i + 1);
         return;
       }
     }
     return;
   }
-
-  for (int i = 0; i < 4; ++i) {
-    if (k > 0 && i == dir[k - 1]) continue;
-    dir[k] = i;
+  for (int d = 0; d < 4; ++d) {
+    if (k > 0 && tilt_d[k - 1] == d) continue;
+    tilt_d[k] = d;
     dfs(k + 1);
   }
 }
@@ -64,9 +88,7 @@ int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  // 입력
   cin >> N >> M;
-
   for (int i = 0; i < N; ++i) {
     for (int j = 0; j < M; ++j) {
       cin >> board[i][j];
@@ -74,8 +96,6 @@ int main(void) {
       else if (board[i][j] == 'B') blue = {i, j};
     }
   }
-
-  // 출력
   dfs(0);
   if (ans == INT_MAX) cout << -1;
   else cout << ans;
@@ -84,7 +104,7 @@ int main(void) {
 /*
 . 빈 칸
 # 벽
-0 구멍
+O 구멍
 R 빨간 구슬
 B 파란 구슬
 */
