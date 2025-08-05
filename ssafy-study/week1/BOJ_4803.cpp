aeb33dba45834c469d85e96876cd5d7db12b74f1
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-08-05 21:43:28 +0900

    BOJ 4803

---
+++ ssafy-study/week1/BOJ_4803.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NODE first
+#define PARENT second
+
+int n, m;
+
+int bfs(const vector<int> (&adj)[501]) {
+  bool vis[501] = {};
+  int cnt = 0;
+
+  // 각 노드별로 트리 찾기
+  for (int i = 1; i <= n; ++i) {
+    if (vis[i]) continue;
+
+    bool is_cycle = 0;
+    queue<pair<int, int>> q;
+
+    q.push({i, 0});
+    vis[i] = 1;
+
+    while (!q.empty()) {
+      pair<int, int> cur = q.front(); q.pop();
+      for (int nxt : adj[cur.NODE]) {
+        if (vis[nxt]) {
+          if (nxt != cur.PARENT) is_cycle = 1;
+          continue;
+        }
+        q.push({nxt, cur.NODE});
+        vis[nxt] = 1;
+      }
+    }
+    if (!is_cycle) ++cnt;
+  }
+  return cnt;
+}
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int tc = 1;
+  while (1) {
+    cin >> n >> m;
+    if (n == 0 && m == 0) break;
+    vector<int> adj[501];
+    
+    for (int i = 0; i < m; ++i) {
+      int u, v;
+      cin >> u >> v;
+      adj[u].push_back(v);
+      adj[v].push_back(u);
+    }
+
+    int cnt = bfs(adj);
+
+    cout << "Case " << tc << ": ";
+    if (cnt == 0) cout << "No trees.\n";
+    else if (cnt == 1) cout << "There is one tree.\n";
+    else cout << "A forest of " << cnt << " trees.\n";
+
+    ++tc;
+  }
+}
