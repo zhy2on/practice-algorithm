58680571a42c64fc2f76bfc654fc5121f196a53b
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-08-12 20:09:09 +0900

    4008 정답. 백트래킹

--- ssafy-study/week2/SWEA_4008.java
+++ ssafy-study/week2/SWEA_4008.java
@@ -7,50 +7,38 @@ public class SWEA_4008 {
   static BufferedReader br;
   static StringBuilder sb = new StringBuilder();
   static int[] num = new int[12];
-  static int[] sign =  new int[11];
-  static int[] selected = new int[11];
-  static boolean[] isused = new boolean[11];
+  static int[] operator = new int[4];
 
-  static void calc() {
-    int ans = num[0];
-    for (int i = 0; i < N - 1; ++i) {
-      if (selected[i] == 0) {
-        ans += num[i + 1];
-      } else if (selected[i] == 1) {
-        ans -= num[i + 1];
-      } else if (selected[i] == 2) {
-        ans *= num[i + 1];
-      } else if (selected[i] == 3) {
-        ans /= num[i + 1];
-      }
-    }
-    maxAns = Math.max(ans, maxAns);
-    minAns = Math.min(ans, minAns);
-  }
-
-  static void dfs(int k) {
+  static void dfs(int k, int result) {
     if (k == N - 1) {
-      calc();
+      minAns = Math.min(result, minAns);
+      maxAns = Math.max(result, maxAns);
       return;
     }
-    for (int i = 0; i < N - 1; ++i) {
-      if (isused[i]) continue;
-      isused[i] = true;
-      selected[k] = sign[i];
-      dfs(k + 1);
-      isused[i] = false;
+    for (int i = 0; i < 4; ++i) { // 각 연산자 중 하나를 사용
+      if (operator[i] == 0) continue;
+
+      --operator[i]; // 연산자 사용
+      if (i == 0) {
+        dfs(k + 1, result + num[k + 1]);
+      } else if (i == 1) {
+        dfs(k + 1, result - num[k + 1]);
+      } else if (i == 2) {
+        dfs(k + 1, result * num[k + 1]);
+      } else if (i == 3) {
+        dfs(k + 1, result / num[k + 1]);
+      }
+      ++operator[i]; // 다시 복귀
     }
   }
 
   static void init() {
-    for (int i = 0; i < N - 1; ++i) isused[i] = false;
     maxAns = Integer.MIN_VALUE;
-    minAns = Integer.MAX_VALUE;
+    minAns = Integer.MAX_VALUE; 
   }
 
   public static void main(String[] args) throws IOException {
-    br = new BufferedReader(new FileReader("ssafy-study/week2/sample_input.txt"));
-    // br = new BufferedReader(new InputStreamReader(System.in));
+    br = new BufferedReader(new InputStreamReader(System.in));
 
     T = Integer.parseInt(br.readLine());
     for (int tc = 1; tc <= T; ++tc) {
@@ -61,17 +49,13 @@ public class SWEA_4008 {
       N = Integer.parseInt(br.readLine());
 
       StringTokenizer st = new StringTokenizer(br.readLine());
-      int signIdx = 0;
-      for (int i = 0; i < 4; ++i) {
-        int n = Integer.parseInt(st.nextToken());
-        while (n-- > 0) sign[signIdx++] = i;
-      }
+      for (int i = 0; i < 4; ++i) operator[i] = Integer.parseInt(st.nextToken());
 
       st = new StringTokenizer(br.readLine());
       for (int i = 0; i < N; ++i) num[i] = Integer.parseInt(st.nextToken());
 
       // 실행
-      dfs(0);
+      dfs(0, num[0]);
 
       // 출력
       sb.append('#').append(tc).append(' ').append(maxAns - minAns).append('\n');
