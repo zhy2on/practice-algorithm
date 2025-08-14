efbadb5f78ecf27d4560e3488647823d8c9feb9d
오지현 <dhwlgus5940@gmail.com>
2025-08-14 15:51:32 +0900

    SWEA 1767 백트래킹

--- ssafy-study/week2/SWEA_1767.java
+++ ssafy-study/week2/SWEA_1767.java
@@ -5,79 +5,63 @@ public class SWEA_1767 {
 
   static BufferedReader br;
   static StringBuilder sb = new StringBuilder();
-  static final int X = 0, Y = 1;
-  static int[][] xy = new int[12][2]; // 코어 좌표
-  static int[] dir = new int[12];
+  static int[][] coreXY = new int[12][2]; // 가장 자리가 아닌 코어 좌표만 저장
   static int[] dx = new int[] {1, 0, -1, 0};
   static int[] dy = new int[] {0, -1, 0, 1};
   static int[][] board = new int[12][12];
-  static int[][] board2 = new int[12][12];
-  static int T, N, coreCnt, maxCore, ans;
+  static int T, N, totalCnt, maxCore, ans;
 
   static int getLineLen(int x, int y, int dir) {
     int len = 0;
     while (true) {
       int nx = x + dx[dir], ny = y + dy[dir];
       if (nx < 0 || nx >= N || ny < 0 || ny >= N) return len; // 마지막까지 도착했다면
-      if (board2[nx][ny] == 1) return -1; // 마지막에 도달 못한 거라면
+      if (board[nx][ny] == 1) return -1; // 마지막에 도달 못한 거라면
       x = nx;
       y = ny;
       ++len;
     }
   }
 
-  static int setLine(int x, int y, int dir) {
-    int len = getLineLen(x, y, dir);
-    if (len < 0) return -1;
+  static void setLine(int x, int y, int dir, int len, int value) {
     for (int i = 0; i < len; ++i) {
       x += dx[dir];
       y += dy[dir];
-      board2[x][y] = 1;
-    }
-    return len;
-  }
-
-  static void simulation() {
-    int powerCore = 0, len = 0;
-    // 시뮬레이션 돌릴 board2 초기화
-    for (int i = 0; i < N; ++i) for (int j = 0; j < N; ++j) board2[i][j] = board[i][j];
-
-    // 정해진 방향에 따라 전선 깔기 시뮬레이션
-    for (int coreIdx = 0; coreIdx < coreCnt; ++coreIdx) {
-      if (dir[coreIdx] == -1) continue;
-      int tmp = setLine(xy[coreIdx][X], xy[coreIdx][Y], dir[coreIdx]);
-      if (tmp < 0) continue;
-      ++powerCore;
-      len += tmp;
-    }
-
-    // 값 업데이트
-    if (powerCore > maxCore) {
-      maxCore = powerCore;
-      ans = len;
-    } else if (powerCore == maxCore) {
-      ans = Math.min(ans, len);
+      board[x][y] = value;
     }
   }
 
-  static void dfs(int k) {
-    if (k == coreCnt) { // 방향이 다 정해지면
-      simulation(); // 시뮬레이션을 돌린다
+  static void dfs(int index, int connectedCnt, int totalLen) {
+    // 가지치기. 남은 걸 모두 더해도 maxCore를 못 넘긴다면 그냥 return
+    if (connectedCnt + (totalCnt - index) < maxCore) return;
+
+    if (index == totalCnt) { // 기저 조건
+      if (connectedCnt > maxCore) { // 최대 연결이라면
+        maxCore = connectedCnt; // maxCore를 바꿔주고
+        ans = totalLen; // 그냥 ans를 totalLen으로 업데이트
+      } else if (connectedCnt == maxCore) { // 연결 수가 같다면
+        ans = Math.min(ans, totalLen); // 더 짧은 길이가 ans
+      }
       return;
     }
 
-    if (dir[k] == -1) { // 이미 전원 연결된 코어는 그냥 건너뛴다
-      dfs(k + 1);
-      return;
-    }
-    for (int i = 0; i < 4; ++i) {
-      dir[k] = i; // 방향을 정해준다
-      dfs(k + 1);
+    // 전선 설치 안 하는 경우
+    dfs(index + 1, connectedCnt, totalLen);
+
+    // 전선 설치하는 경우
+    for (int d = 0; d < 4; ++d) { // 4방향 전선 설치 시도
+      int x = coreXY[index][0], y = coreXY[index][1];
+      int len = getLineLen(x, y, d);
+      if (len < 0) continue; // 전선 설치할 수 없으면 continue
+
+      setLine(x, y, d, len, 1); // 전선 설치
+      dfs(index + 1, connectedCnt + 1, totalLen + len); // 설치하고 들어감
+      setLine(x, y, d, len, 0); // 다시 복구
     }
   }
 
   static void init() {
-    coreCnt = 0;
+    totalCnt = 0;
     maxCore = 0;
     ans = Integer.MAX_VALUE;
   }
@@ -104,15 +88,14 @@ public class SWEA_1767 {
         for (int j = 0; j < N; ++j) {
           board[i][j] = st.nextToken().charAt(0) - '0';
           if (board[i][j] == 1) {
-            if (i == N - 1 || j == N - 1) dir[coreCnt] = -1;
-            else dir[coreCnt] = 0;
-            xy[coreCnt++] = new int[] {i, j};
+            if (i == N - 1 || j == N - 1) continue; // 가장자리라면 넣을 필요 없다
+            coreXY[totalCnt++] = new int[] {i, j};
           }
         }
       }
 
       // 실행
-      dfs(0);
+      dfs(0, 0, 0);
 
       // 출력
       sb.append('#').append(tc).append(' ').append(ans).append('\n');
@@ -120,15 +103,3 @@ public class SWEA_1767 {
     System.out.print(sb);
   }
 }
-
-/*
-3
-7
-0 0 1 0 0 0 0
-0 0 1 0 0 0 0
-0 0 0 0 0 1 0
-0 0 0 0 0 0 0
-1 1 0 1 0 0 0
-0 1 0 0 0 0 0
-0 0 0 0 0 0 0
- */
