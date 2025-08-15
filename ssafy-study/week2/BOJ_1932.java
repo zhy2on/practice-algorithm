f9c967c3d9f6968525836ab9dfc6913f83df420c
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-08-15 19:23:55 +0900

    BOJ 1932 회의실 배정 ver2 - 시간초과

--- ssafy-study/week2/BOJ_1932.java
+++ ssafy-study/week2/BOJ_1932.java
@@ -6,35 +6,33 @@ public class BOJ_1932 {
   static BufferedReader br;
   static StringBuilder sb = new StringBuilder();
   static int N, ans;
-  static List<Meeting> meetList = new ArrayList<>();
-  static int[] cnt = new int[1000000];
-
-  static class Meeting {
-    int start, end;
-    Meeting(int start, int end) { this.start = start; this.end = end; }
-  }
+  static int[][] meet = new int[1000000][2];
+  static boolean[] isused = new boolean[1000000];
 
   static void run() {
     // 정렬 - 시작시간순. 시작시간이 같다면 종료시간이 더 빠른 걸로
-    Collections.sort(meetList, (m1, m2) -> {
-      if (m1 != m2) return m1.start - m2.start;
-      return m1.end - m2.end;
+    Arrays.sort(meet, 0, N, (a, b) -> {
+      if (a[0] != b[0]) return a[0] - b[0];
+      return a[1] - b[1];
     });
 
     // 해당 회의가 시작일 때의 최대 회의 개수 구하기
     for (int i = 0; i < N; ++i) {
-      int cur = meetList.get(i).end;
+      int cnt = 1;
+
+      if (isused[i]) continue;
+      int cur = meet[i][1]; // 끝나는 시간
       int j = i + 1;
       while (true) {
-        while (j < N && meetList.get(j).start < cur) ++j;
+        while (j < N && meet[j][0] < cur) ++j; // 시작시간이 끝나는 시간 이전이면 다음 걸 탐색
         if (j == N) break;
-        cur = meetList.get(j).end;
-        ++cnt[i];
+        cur = meet[j][1]; // 새로운 cur은 끝나는 시간
+        isused[j] = true;
+        ++cnt;
       }
-    }
 
-    // max값으로 업데이트
-    ans = Arrays.stream(cnt).max().getAsInt() + 1;
+      ans = Math.max(cnt, ans);
+    }
   }
 
   public static void main(String[] args) throws IOException {
@@ -45,7 +43,7 @@ public class BOJ_1932 {
       StringTokenizer st = new StringTokenizer(br.readLine());
       int start = Integer.parseInt(st.nextToken());
       int end = Integer.parseInt(st.nextToken());
-      meetList.add(new Meeting(start, end));
+      meet[i] = new int[]{start, end};
     }
 
     run();
