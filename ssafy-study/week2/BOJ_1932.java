5c9caaaf4a0e8e922efaa44edfd274e6386ecfe0
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-08-15 20:00:19 +0900

    BOJ 1932 회의실 배정 - 그리디

--- ssafy-study/week2/BOJ_1932.java
+++ ssafy-study/week2/BOJ_1932.java
@@ -7,24 +7,24 @@ public class BOJ_1932 {
   static StringBuilder sb = new StringBuilder();
   static int N, ans;
   static int[][] meet = new int[1000000][2];
-  static int[] cnt = new int[1000000];
 
   static void run() {
-    // 정렬 - 시작시간순. 시작시간이 같다면 종료시간이 더 빠른 걸로
+    // 정렬 - 끝나는 시간이 기준이 돼야함. 시작시간이 끝나는 시간에 종속되어 있기 때문!
     Arrays.sort(meet, 0, N, (a, b) -> {
-      if (a[0] != b[0]) return a[0] - b[0];
-      return a[1] - b[1];
+      // 먼저 종료시간 순으로 정렬
+      if (a[1] != b[1]) return a[1] - b[1];
+      // 그 다음 시작시간 순으로 정렬 - 종료시간과 시작시간이 같을 수 있기 때문에 필요하다!!
+      // ex) [(2, 2), (1, 2)] 가 있을 때
+      // (1, 2)를 사용하고 (2, 2)를 사용할 수 있기 때문에 최대 2개를 사용할 수 있지만
+      // 시작시간 기준으로 정렬이 적용되지 않았다면 (2, 2)를 먼저 선택해버려서 (1, 2)가 선택되지 않는다
+      return a[0] - b[0];
     });
 
-    // 해당 회의가 시작일 때의 최대 회의 개수 구하기
-    cnt[0] = 1;
-    for (int i = 1; i < N; ++i) {
-      cnt[i] = 1;
-      int j = i - 1;
-      while (j >= 0 && meet[j][1] > meet[i][0]) --j;
-      if (j == -1) continue;
-      cnt[i] += cnt[j];
-      ans = Math.max(ans, cnt[i]);
+    int cur = 0;
+    for (int i = 0; i < N; ++i) {
+      if (meet[i][0] < cur) continue;
+      ++ans;
+      cur = meet[i][1];
     }
   }
 
