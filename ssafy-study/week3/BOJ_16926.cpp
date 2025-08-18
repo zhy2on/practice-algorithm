0a7fe7804b2ac758cdd53403febeeb03cf405069
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-08-18 22:48:06 +0900

    BOJ 16926 배열 돌리기 1 - 배열 평탄화, 인덱스 계산

--- ssafy-study/week3/BOJ_16926.cpp
+++ ssafy-study/week3/BOJ_16926.cpp
@@ -15,11 +15,11 @@ bool rotateLayer(int l, int r) { // layer, rotate
 
   int size = n * 2 + m * 2 - 4;
 
+  // 평탄화 배열을 보드 시계방향으로 채운다.
   int d = 0;
   int bx = l, by = l; // board상 레이어의 시작점은 l, l
   int idx = 0;
   int cnt = 0;
-  // 평탄화 배열을 보드 시계방향으로 채운다.
   while (cnt < size) {
     tmp[idx++] = board[bx][by];
     bx += dx[d];
@@ -30,22 +30,16 @@ bool rotateLayer(int l, int r) { // layer, rotate
     else if (cnt == m - 1 + n - 1 + m - 1) ++d;
   }
 
-  // cout << "L: " << l << '\n';
-  // for (int i = 0; i < size; ++i) cout << tmp[i] << ' ';
-  // cout << '\n';
-
   // 평탄화 배열을 다시 2차원 보드에 덮어씌운다. 평탄화 배열 r번째가 layer 시작점 => r % size임!!!!!!!
   d = 0;
   bx = l, by = l;
   idx = r % size;
-  // cout << "tmp[r]: " << tmp[r] << '\n';
   cnt = 0;
   while (cnt < size) {
     board[bx][by] = tmp[idx];
     idx = (idx + 1) % size;
     bx += dx[d];
     by += dy[d];
-    // cout << "bx, by, board: " << bx << " " << by << " " << board[bx][by] << "\n";
     ++cnt;
     if (cnt == m - 1) ++d;
     else if (cnt == m - 1 + n - 1) ++d;
@@ -69,8 +63,6 @@ int main(void) {
   }
 
   rotate(R);
-
-  // cout << '\n';
 
   for (int i = 0; i < N; ++i) {
     for (int j = 0; j < M; ++j) cout << board[i][j] << ' ';
