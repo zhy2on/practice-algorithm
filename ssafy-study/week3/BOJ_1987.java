6f3335cc2e834a9e26a9e03cca8f59fefd6b63d2
오지현 <dhwlgus5940@gmail.com>
2025-08-20 16:36:29 +0900

    BOJ 1987 알파벳 - dfs, 비트마스킹

--- ssafy-study/week3/BOJ_1987.java
+++ ssafy-study/week3/BOJ_1987.java
@@ -7,19 +7,17 @@ public class BOJ_1987 {
   static int R, C, ans;
   static char[][] board = new char[22][22];
   static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-  static boolean[] alpha = new boolean[26];
 
-  static void dfs(int x, int y, int cnt) {
+  static void dfs(int x, int y, int cnt, int mask) {
     ans = Math.max(ans, cnt); // 정답 업데이트
     for (int d = 0; d < 4; ++d) { // 방향별로
       int nx = x + dir[d][0], ny = y + dir[d][1];
       if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
-      if (alpha[board[nx][ny] - 'A']) continue;
 
-      // 갈 수 있다면
-      alpha[board[nx][ny] - 'A'] = true; // 값 업데이트하고
-      dfs(nx, ny, cnt + 1); // 들어가고
-      alpha[board[nx][ny] - 'A'] = false; // 나와서 다시 복구 
+      int bit = 1 << (board[nx][ny] - 'A'); // nx, ny번 방문 비트
+      if ((mask & bit) != 0) continue; // 이미 켜져 있었으면 continue
+
+      dfs(nx, ny, cnt + 1, mask | bit); // 새로운 알파벳 방문 비트 켜서 넘기기
     }
   }
 
@@ -32,8 +30,7 @@ public class BOJ_1987 {
 
     for (int i = 0; i < R; ++i) board[i] = br.readLine().toCharArray();
 
-    alpha[board[0][0] - 'A'] = true;
-    dfs(0, 0, 1);
+    dfs(0, 0, 1, 1 << (board[0][0] - 'A')); // 0, 0번 방문 비트 켜기
     System.out.print(ans);
   }
 }
